d00d446546502b127a67e3fd3db085ed0905166a
Gabriel Leme <gabriellemenw@gmail.com>
2024-06-09 16:02:32 -0300

    testando valores de balanceamento

--- Assets/_Project/Scripts/Character/Towers/Tower.cs
+++ Assets/_Project/Scripts/Character/Towers/Tower.cs
@@ -1,9 +1,7 @@
-using System.Collections;
 using System.Collections.Generic;
 using TMPro;
 using UnityEngine;
 
-using UnityEngine.UI;
 public abstract class Tower : Entity
 {
     [SerializeField] protected float fireRate = 1f;
--- Assets/_Project/Scripts/Character/Towers/TowerMilho.cs
+++ Assets/_Project/Scripts/Character/Towers/TowerMilho.cs
@@ -1,12 +1,7 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class TowerMilho : Tower
 {
-
-
-
     public override void Start()
     {
         base.Start();
